a3776755ad75bf25f26abf8f49cf127a0b793853
fawazmypotpot-hash <fawazmypotpot@gmail.com>
2026-05-30 12:07:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logging.basicConfig(
 # قراءة التوكن من متغير البيئة (آمن)
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
-    raise ValueError("8687541181:AAGImfmybwuBWOoH6BtxSVtSOD8zfquQ4-A")
+    raise ValueError("8687541181:AAH3ep_dhi6-3jD36v7dqyylanJ7YOWWmHY")
 # استقبال روابط يوتيوب وتنزيل الفيديو
 async def handle_youtube(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text.strip()
